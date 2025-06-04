0924618d0607d50eb0c3b46c94defaeb9036a83f
TheBlueFriend <adrishg3@gmail.com>
2025-06-04 22:27:41 +0530

    Vercel push

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
+import { ThemeProvider } from '@/app/(app)/ThemeContext'; // Adjust path to your ThemeProvider
 import FirebaseProvider from '@/components/FirebaseProvider';
 import UserDataProvider from '@/components/UserDataProvider';
-import { ThemeProvider } from './(app)/ThemeContext'; // Adjust path to your ThemeProvider
 import { Slot } from "expo-router";
 
 export default function RootLayout() {
